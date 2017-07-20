3f04bca872e9cb2b5ae9dc866059c4279ec80ea8
Ilya_Myalik <Ilya_Myalik@epam.com>
2017-07-20 22:21:40 +0400

    Finish

---
+++ IMQ1.HTTPFundamentation/IMQ1.HTTP.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using IMQ1.HTTP.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMQ1.HTTP.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string urlPath = "https://calendar.google.com/";
+            const string folderPath = @"D:\SomethingGoesGOOD\";
+
+            var callTask = Task.Run(() => CustomWget.DownloadAsync(urlPath, folderPath, 2, DomainRestriction.NoRestriction, true));
+            callTask.Wait();
+        }
+    }
+}
---
+++ IMQ1.HTTPFundamentation/IMQ1.HTTP.Library/ConsoleNotifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMQ1.HTTP.Library
+{
+    public class ConsoleNotifier : INotifier
+    {
+        public void Notify(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
